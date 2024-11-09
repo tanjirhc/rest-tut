2cf8fff3383ccb74c4c3ffd181923e1f4ac239eb
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-09 17:02:24 +0600

    nodemon install to watch server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('<h1>Hello World</h1>')
 })
 
 app.listen(PORT, () => {
