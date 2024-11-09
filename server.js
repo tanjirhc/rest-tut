d8db7e97912468f6691e44ec9ad7ea9f15e68349
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-09 17:19:15 +0600

    posts page router

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
+  res.send('<div><h1>Hello World</h1><p>Hello Fleckians!</p></div>')
+})
+
+app.get('/posts', (req, res) => {
+  res.send('<h1>I am post page</h1>')
 })
 
 app.listen(PORT, () => {
