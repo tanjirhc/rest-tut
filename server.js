c10189d8f430141484abfdcd9a0937e63772a356
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-12 18:53:15 +0600

    api route edit

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000
 
+app.use('/api/contacts', contactRoute)
+
 app.get('/', (req, res) => {
   res.send('<div><h1>Hello World</h1><p>Hello Fleckians!</p></div>')
 })
 
-app.use('/api/contacts', contactRoute)
+app.listen(PORT, () => {
+  console.log(`Server is Running on PORT ${PORT}`);
+})
 
 const contacts = [
   {name: 'HM Nayem', email: 'something@gmail.com'},
