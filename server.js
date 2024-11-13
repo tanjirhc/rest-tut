dc88b1f077ee74c637ee22526edd35a06f8fbe5e
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-11-13 18:47:55 +0600

    nodeman crashed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const contactRoute = require('./api/routes/contact')
 
-const app = express();
+const app = express()
 
 const PORT = process.env.PORT || 3000
 
